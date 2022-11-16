644c90092b77116cc420f0535b970b49e75bc858
ivanhack34 <ivanpolanco34@gmail.com>
2022-11-15 21:46:27 -0400

    Mi programa de hola mundo

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const port = 9000
 
 const app = express()
 
+app.get('/hola', (peticion, respuesta) => {
+    respuesta.json({
+        message: 'Hola mundo'
+    })
+})
+
+app.delete('/hola', (peticion, respuesta) => {
+    respuesta.json({
+        message: 'Hola pero desde delete'
+    })
+})
+
+
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
 })
